320441b1a82200f03be85c6ca88dc832385a6d04
dan <dan@DESKTOP-HHK66MJ>
2020-01-24 11:15:17 +0600

    complete commit

---
+++ app/js/scripts.js
@@ -0,0 +1,22 @@
+let toggler = document.querySelector('.toggle__button');
+let close = document.querySelector('.close-button')
+let backdrop = document.querySelector('.backdrop');
+let mobileMenu =document.querySelector('.mobile-nav');
+
+toggler.addEventListener('click', function(e) {
+    backdrop.style.display = "block";
+    this.style.display = "none";
+    mobileMenu.classList.add('mobile-nav-show');
+});
+
+close.addEventListener('click', function(e) {
+    backdrop.style.display = "none";
+    toggler.style.display = "block";
+    mobileMenu.classList.remove('mobile-nav-show');
+});
+
+backdrop.addEventListener('click', function() {
+    backdrop.style.display = "none";
+    toggler.style.display = "block";
+    mobileMenu.classList.remove('mobile-nav-show');
+})
